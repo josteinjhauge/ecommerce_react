924d811022b3758bd24004e55bae6869481fa81d
Jostein <josteinjhauge@me.com>
2020-12-11 11:04:49 +0100

    shop components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './header.scss';
+
+const Header = () => (
+    <div className='header'>
+        <Link to='/'>
+            
+        </Link>
+    </div>
+)
