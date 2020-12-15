4ecf58da4c984ee49dbdcc9148d7c41ae6a39064
Jostein <josteinjhauge@me.com>
2020-12-15 12:31:12 +0100

    firestore and storing autheticated users

--- src/pages/signIn/signInPage.jsx
+++ src/pages/signIn/signInPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './signInPage.scss';
 import SingIn from '../../components/sign-in/signInComponent';
+import SignUp from '../../components/sign-up/sign-up';
 
 
 const SignInPage = () => (
     <div className='sign-in-and-sign-up'>
         <SingIn />
+        <SignUp />
     </div>
 );
 
