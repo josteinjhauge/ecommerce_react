6db7b8e5293129c05cb308d7d5b87ae8fe9eecf9
Jostein <josteinjhauge@me.com>
2020-12-16 12:11:09 +0100

    redux to add items into array

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -3,14 +3,23 @@ import './menu-item.scss';
 import { withRouter } from "react-router-dom";
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
-    <div className={`${size} menu-item`} 
-    onClick ={() => history.push(`${match.url}${linkUrl}`)}>
-        <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-        }}/>
+    <div 
+        className={`${size} menu-item`} 
+        onClick ={() => 
+            history.push(`${match.url}${linkUrl}`)}
+    >
+        <div 
+            className='background-image' 
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}/>
         <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>Kjøp nå</span>
+            <h1 className='title'>
+                {title.toUpperCase()}
+            </h1>
+            <span className='subtitle'>
+                Kjøp nå
+            </span>
         </div>
     </div>
 );
